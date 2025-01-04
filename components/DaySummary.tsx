4e08509a415d2c1169cdc38399d40024137111ee
Eranda Bandaranaike <ergroove@live.com>
2025-01-04 15:27:54 +0530

    Date range for the day summary added

--- components/DaySummary.tsx
+++ components/DaySummary.tsx
@@ -1,50 +1,83 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "@/lib/axios";
 import TotalRevenue from "@/components/reports/TotalRevenue";
 import RevenueByDoctor from "@/components/reports/RevenueByDoctor";
 import BillSummary from "@/components/reports/BillSummary";
 import DailyReportSummary from "@/components/reports/DailyReportSummary";
-import {BillSummaryData, DailyReportSummaryData, dailyReportSummaryProps, RevenueByDoctorData, TotalRevenueData, TotalRevenueProps} from "@/types/report-interfaces";
+import {DateRangePicker, RangeValue, DateValue} from "@nextui-org/react";
+import dayjs from "dayjs";
 
+import {BillSummaryData, DailyReportSummaryData, RevenueByDoctorData, TotalRevenueData} from "@/types/report-interfaces";
+import {backgroundColor} from "html2canvas/dist/types/css/property-descriptors/background-color";
+
+interface DateRange {
+    startDate: string | null;
+    endDate: string | null;
+}
 
 const DaySummary = () => {
-    const [totalRevenue, setTotalRevenue] = useState<TotalRevenueData | undefined>();
-    const [revenueByDoctor, setRevenueByDoctor] = useState<RevenueByDoctorData[] | undefined>();
     const [billStatusSummary, setBillStatusSummary] = useState<BillSummaryData | undefined>();
     const [dailyReportSummary, setDailyReportSummary] = useState<DailyReportSummaryData | undefined>();
+    const [revenueByDoctor, setRevenueByDoctor] = useState<RevenueByDoctorData[] | undefined>();
+    const [totalRevenue, setTotalRevenue] = useState<TotalRevenueData | undefined>();
+    const [startDate, setStartDate] = useState<string | null>();
+    const [endDate, setEndDate] = useState<string | null>();
 
-    // Fetch data from the API on component mount
+    let isDataLoaded = false;
+    const fetchData = async () => {
+        try {
+            const response = await axios.get("reports", {data: {startDate, endDate}});
+            const {
+                billStatusSummary,
+                dailyReportSummary,
+                revenueByDoctor,
+                totalRevenue,
+            } = response.data;
+
+            setBillStatusSummary(billStatusSummary);
+            setDailyReportSummary(dailyReportSummary);
+            setRevenueByDoctor(revenueByDoctor);
+            setTotalRevenue(totalRevenue);
+            isDataLoaded = true;
+        } catch (error) {
+            console.error("Failed to fetch reports data:", error);
+        }
+    };
+
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get("reports");
-                const {
-                    totalRevenue,
-                    revenueByDoctor,
-                    billStatusSummary,
-                    dailyReportSummary,
-                } = response.data;
-
-                setTotalRevenue(totalRevenue);
-                setRevenueByDoctor(revenueByDoctor);
-                setBillStatusSummary(billStatusSummary);
-                setDailyReportSummary(dailyReportSummary);
-            } catch (error) {
-                console.error("Failed to fetch reports data:", error);
-            }
+        return () => {
+            if (!isDataLoaded) fetchData();
         };
+    }, [startDate, endDate]);
 
-        fetchData();
-    }, []); // Empty dependency array ensures this runs on component mount
+    // // Fetch data from the API on component mount
+    // useEffect(() => {
+    //     return () => {
+    //         if (!isDataLoaded) fetchData();
+    //     };
+    // }, []); // Empty dependency array ensures this runs on component mount
+
+    const handleDateChange = (value: RangeValue<DateValue> | null) => {
+        const formattedStartDate = value?.start ? dayjs(value.start.toDate('Asia/Colombo')).format("YYYY-MM-DD") : null;
+        const formattedEndDate = value?.end ? dayjs(value.end.toDate('Asia/Colombo')).format("YYYY-MM-DD") : null;
+        setStartDate(formattedStartDate);
+        setEndDate(formattedEndDate);
+    };
 
     return (
         <div>
-            <h2 className="font-semibold text-xl">Day Summary Component</h2>
-            <p className="mb-8">
-                This dashboard provides a concise and visually appealing summary of the day&apos;s performance
-                trends.
-            </p>
-
+            <div className="flex justify-between">
+                <div>
+                    <h2 className="font-semibold text-xl">Day Summary Component</h2>
+                    <p className="mb-8">
+                        This dashboard provides a concise and visually appealing summary of the day&apos;s performance and
+                        trends.
+                    </p>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <DateRangePicker className="bg-transparent" onChange={handleDateChange}/>
+                </div>
+            </div>
             <div className="grid grid-cols-6 gap-3">
                 <div className="col-span-4">
                     <DailyReportSummary data={dailyReportSummary}/>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,26 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
+import {nextui} from "@nextui-org/react";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/flowbite-react/lib/**/*.js",
-    "./node_modules/tailwind-datepicker-react/dist/**/*.js",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./node_modules/flowbite-react/lib/**/*.js",
+        "./node_modules/tailwind-datepicker-react/dist/**/*.js",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
     },
-  },
-  plugins: [
-    require("flowbite/plugin")
-  ],
+    plugins: [
+        require("flowbite/plugin"), nextui()
+    ],
 };
 export default config;
